963001d385ecf3b2bdaf83e329269b61ca3cc3f1
Qingcheng Zhou <zhouqc@uuxoo.com>
2021-03-01 08:44:56 +0800

    finish creating fractal

---
+++ Assets/Scripts/Fractal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour
+{
+    [SerializeField, Range(1, 8)]
+    int depth = 4;
+
+    void Start()
+    {
+        name = "Fractal " + depth;
+        if (depth <= 1)
+        {
+            return;
+        }
+
+        Fractal childA = CreateChild(Vector3.up);
+        Fractal childB = CreateChild(Vector3.right);
+
+        childA.transform.SetParent(transform, false);
+        childB.transform.SetParent(transform, false);
+    }
+
+    Fractal CreateChild(Vector3 direction) {
+        Fractal child = Instantiate(this);
+        child.depth = depth - 1;
+        child.transform.localPosition = 0.75f * direction;
+        child.transform.localScale = 0.5f * Vector3.one;
+        return child;
+    }
+}
