90f860e1af1fda2282ed831561a659d962368423
Qingcheng Zhou <zhouqc@uuxoo.com>
2021-03-01 09:44:39 +0800

    finish reorientation

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -15,17 +15,24 @@ public class Fractal : MonoBehaviour
             return;
         }
 
-        Fractal childA = CreateChild(Vector3.up);
-        Fractal childB = CreateChild(Vector3.right);
+        Fractal childA = CreateChild(Vector3.up, Quaternion.identity);
+        Fractal childB = CreateChild(Vector3.right, Quaternion.Euler(0f, 0f, -90f));
+        Fractal childC = CreateChild(Vector3.left, Quaternion.Euler(0f, 0f, 90f));
+        Fractal childD = CreateChild(Vector3.forward, Quaternion.Euler(90f, 0f, 0f));
+        Fractal childE = CreateChild(Vector3.back, Quaternion.Euler(-90f, 0f, 0f));
 
         childA.transform.SetParent(transform, false);
         childB.transform.SetParent(transform, false);
+        childC.transform.SetParent(transform, false);
+        childD.transform.SetParent(transform, false);
+        childE.transform.SetParent(transform, false);
     }
 
-    Fractal CreateChild(Vector3 direction) {
+    Fractal CreateChild(Vector3 direction, Quaternion rotation) {
         Fractal child = Instantiate(this);
         child.depth = depth - 1;
         child.transform.localPosition = 0.75f * direction;
+        child.transform.localRotation = rotation;
         child.transform.localScale = 0.5f * Vector3.one;
         return child;
     }
